66ebdcae154f5efed3592690527dfab79a693226
Josh R <jriver35@uncc.edu>
2020-05-13 19:49:31 -0400

    Added dynamic functionality to main card

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,69 @@
 // -------SEARCH HISTORY FORM------
 // WHEN THE USER SEARCHERS FOR A CITY BY CLICKING THE SEARCH BUTTON OR PRESSING ENTER
 // PREVENT EVENT DEFAULT
-// SEND LOCATION TO MAIN CARD
-// ADD SEARCH TO SEARCH HISTORY AND STORE IN LOCAL STORAGE
+// GET CITY INPUT AND ASSIGN IT TO A VARIABLE
+//
 // PREPEND EACH SEARCH
 // -------MAIN CARD-------
 // WHEN THE USER SEARCHERS FOR A CITY BY CLICKING THE SEARCH BUTTON OR PRESSING ENTER
-// LOAD MAIN CARD--- LOAD CITY NAME, DATE, AND WEATHER ICON--- LOAD TEMP, HUM, WINDSPEED, AND UV INDEX FROM API
+// LOAD MAIN CARD--- LOAD CITY NAME, DATE, WEATHER ICON, LOAD TEMP, HUM, WINDSPEED, AND UV INDEX FROM API
 // ------5-DAY FORECAST-----
 // WHEN THE USER SEARCHERS FOR A CITY BY CLICKING THE SEARCH BUTTON OR PRESSING ENTER
 // lOAD 5-DAY CARDS--- DYNAMICALLY LOAD DATE AND WEATHER ICONS --- RETREIVE TEMP AND HUMIDITY FROM API AND DISPLAY
+
+// create on submit for form
+$("#searchBtn").on("click", function () {
+  event.preventDefault();
+  var userCity = $("#city-search").val().trim();
+
+  $(".card-header").text(userCity);
+
+  //   var searchHistoryArr = [];
+
+  //   // add each search to seach history
+  //   for (var i = 0; i < searchHistoryArr.length; i++) {
+  //     var searchHistoryDiv = $("<div>");
+  //     var searchHistoryText = $("<p>");
+
+  //     searchHistoryText.text("<hr>" + searchHistoryArr[i] + "<hr>");
+  //     searchHistoryText.attr("data-city", searchHistoryArr[i]);
+
+  //     searchHistoryDiv.append(searchHistoryText);
+  //     $("#seach-history").prepend(searchHistoryDiv);
+  //   }
+
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    userCity +
+    "&appid=ef720fca1b9f6063f3226146f04c9dfc";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    var temp = parseFloat(response.main.temp);
+    var humidity = parseInt(response.main.humidity);
+    var windSpeed = parseFloat(response.wind.speed);
+    var lat = parseFloat(response.coord.lat);
+    var lon = parseFloat(response.coord.lon);
+
+    $.ajax({
+      url:
+        "http://api.openweathermap.org/data/2.5/uvi?appid=ef720fca1b9f6063f3226146f04c9dfc&lat=" +
+        lat +
+        "&lon=" +
+        lon,
+      method: "GET",
+    }).then(function (response) {
+      var uvIndex = response.value;
+      $("#display-uv").text("UV Index: " + uvIndex);
+    });
+
+    // calculate temperature
+    var fTemp = Math.floor(temp - 273.15) * 1.8 + 32;
+    $("#display-temp").text("Temperature: " + fTemp);
+    $("#display-hum").text("Humidity: " + humidity);
+    $("#display-wind").text("Wind Speed: " + windSpeed);
+  });
+});
